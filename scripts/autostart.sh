a72514d9604f6cc347ce22750e2b2d0aa35ff1cb
0xnofoot <jnwang99@outlook.com>
2022-11-12 02:13:41 +0800

    change border color

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,6 +4,7 @@ sh $DWM_HOME/scripts/dualmon.sh
 sh $DWM_HOME/scripts/dwm-status.sh &
 sh $DWM_HOME/scripts/wp-autochange.sh &
 picom --experimental-backends --config $PICOM_HOME/picom.conf & 
+# picom -b
 xfce4-power-manager &
 xfce4-volumed-pulse &
 sh $DWM_HOME/scripts/run-mailsync.sh &
